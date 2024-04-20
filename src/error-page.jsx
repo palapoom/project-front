2808fbc9a739b9358de4cff1874d1127eb720188
palapoom <palapoom.t@ku.th>
2024-04-21 03:03:40 +0700

    error-page

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -5,11 +5,22 @@ export default function ErrorPage() {
   console.error(error)
 
   return (
-    <div id='error-page'>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
+    <div id='error-page' className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='mb-4 text-5xl font-semibold text-red-500'>WeScrim!</h1>
+      <h1 className='mb-4 text-6xl font-semibold text-red-500'>404</h1>
+      <p className='mb-4 text-xs text-gray-500'>{error.error.message}</p>
+      <p className='mb-4 text-lg text-gray-600'>Oops! Looks like you're lost.</p>
+      <div className='animate-bounce'>
+        <svg className='mx-auto h-16 w-16 text-red-500' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M12 19l9 2-9-18-9 18 9-2zm0 0v-8'></path>
+        </svg>
+      </div>
+      <p className='mt-4 text-gray-600'>
+        Let's get you back{' '}
+        <a href='/' className='text-blue-500'>
+          home
+        </a>
+        .
       </p>
     </div>
   )
